9f0517b17e79783b8db0274fe2ffbe29ad0a6ec6
ibrahim buru <buru2425@gmail.com>
2023-01-21 19:24:34 +0300

    first commit

---
+++ TurkceSozluk/App.js
@@ -0,0 +1,11 @@
+import react from 'react';
+import { Text, View } from 'react-native';
+
+export default function App() {
+    return(
+        <View  style={{flex:1,justifyContent:"center",alignItems:'center'}}>
+            <Text>
+            asdad</Text>
+        </View>
+    )
+}
